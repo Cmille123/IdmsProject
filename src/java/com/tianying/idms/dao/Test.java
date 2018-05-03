967f516bad4b9a2bbfb5e5cc6f5e16f13af56042
pokerface <876441845@qq.com>
2018-05-03 08:57:53 +0800

    test

---
+++ src/java/com/tianying/idms/dao/Test.java
@@ -0,0 +1,13 @@
+package com.tianying.idms.dao;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author: poker face
+ * @date: 2018/5/3 0003 8:57
+ * @since: JDK1.8.0_144
+ * @version: X
+ * Description:
+ */
+public interface Test {
+}
