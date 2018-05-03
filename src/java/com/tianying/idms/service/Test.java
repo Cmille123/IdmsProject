d2aa1f3e11a7c18253b18d68a487937f09180ea5
pokerface <876441845@qq.com>
2018-05-03 08:59:13 +0800

    test

---
+++ src/java/com/tianying/idms/service/Test.java
@@ -0,0 +1,13 @@
+package com.tianying.idms.service;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author: poker face
+ * @date: 2018/5/3 0003 8:59
+ * @since: JDK1.8.0_144
+ * @version: X
+ * Description:
+ */
+public interface Test {
+}
