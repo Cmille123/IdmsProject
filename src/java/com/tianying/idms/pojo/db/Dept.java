cf3802e7661079d8cbfd6876b23a39ce5f813906
pokerface <876441845@qq.com>
2018-05-09 09:17:07 +0800

    test

---
+++ src/java/com/tianying/idms/pojo/db/Dept.java
@@ -0,0 +1,13 @@
+package com.tianying.idms.pojo.db;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author: poker face
+ * @date: 2018/5/9 0009 9:14
+ * @since: JDK1.8.0_144
+ * @version: X
+ * Description:
+ */
+public class Dept {
+}
